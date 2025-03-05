c170be2f5e5821aa55ea7d9f477b2ae96fa47e3f
Vhirazhr <fakhirazahra88@gmail.com>
2025-03-05 23:04:14 +0700

    JS4 - $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,13 +12,14 @@ class UserController extends Controller{
         {
             // Tambah data user dengan Eloquent Model
             $data = [
-              //  'username' => 'customer-1',
-                'nama' => 'Pelanggan Pertama',
-              //  'password' => Hash::make('12345'),
-               // 'level_id' => 4
+                'level_id' => 2,
+                'username' => 'manager_tiga',
+                'nama' => 'Manager 3',
+                'password' => Hash::make('12345'),
+                
             ];
-               UserModel::where('username', 'customer-1')->update($data);
-               // UserModel::insert($data);
+              // UserModel::where('username', 'customer-1')->update($data);
+               UserModel::create($data);
             // Coba akses model UserModel
             $user = UserModel::all(); // Ambil semua data dari tabel m_user
             return view('user', ['data' => $user]);
