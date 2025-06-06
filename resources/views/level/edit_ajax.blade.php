79a86ef521956c48949736de96b23aeb2f4935c5
Fakhira zahrany <fakhirazahra88@gmail.com>
2025-06-06 16:09:52 +0700

    JS6 - Update Ajax

--- resources/views/level/edit_ajax.blade.php
+++ resources/views/level/edit_ajax.blade.php
@@ -1,4 +1,4 @@
-@empty($kategori)
+@empty($level)
     <div id="modal-master" class="modal-dialog modal-md" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -10,20 +10,20 @@
             <div class="modal-body">
                 <div class="alert alert-danger">
                     <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
-                    Data kategori tidak ditemukan
+                    Data level tidak ditemukan
                 </div>
-                <a href="{{ url('/kategori') }}" class="btn btn-warning">Kembali</a>
+                <a href="{{ url('/level') }}" class="btn btn-warning">Kembali</a>
             </div>
         </div>
     </div>
 @else
-    <form action="{{ url('/kategori/' . $kategori->kategori_id . '/update_ajax') }}" method="POST" id="form-edit">
+    <form action="{{ url('/level/' . $level->level_id . '/update_ajax') }}" method="POST" id="form-edit">
         @csrf
         @method('PUT')
         <div id="modal-master" class="modal-dialog modal-md" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Edit Data Kategori</h5>
+                    <h5 class="modal-title">Edit Data Level</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -31,17 +31,17 @@
 
                 <div class="modal-body">
                     <div class="form-group">
-                        <label>Kode Kategori</label>
-                        <input type="text" name="kategori_kode" id="kategori_kode" class="form-control"
-                               value="{{ $kategori->kategori_kode }}" required>
-                        <small id="error-kategori_kode" class="error-text form-text text-danger"></small>
+                        <label>Kode Level</label>
+                        <input type="text" name="level_kode" id="level_kode" class="form-control"
+                               value="{{ $level->level_kode }}" required>
+                        <small id="error-level_kode" class="error-text form-text text-danger"></small>
                     </div>
 
                     <div class="form-group">
-                        <label>Nama Kategori</label>
-                        <input type="text" name="kategori_nama" id="kategori_nama" class="form-control"
-                               value="{{ $kategori->kategori_nama }}" required>
-                        <small id="error-kategori_nama" class="error-text form-text text-danger"></small>
+                        <label>Nama Level</label>
+                        <input type="text" name="level_nama" id="level_nama" class="form-control"
+                               value="{{ $level->level_nama }}" required>
+                        <small id="error-level_nama" class="error-text form-text text-danger"></small>
                     </div>
                 </div>
 
@@ -57,8 +57,8 @@
         $(document).ready(function () {
             $("#form-edit").validate({
                 rules: {
-                    kategori_kode: { required: true, minlength: 2, maxlength: 10 },
-                    kategori_nama: { required: true, minlength: 3, maxlength: 100 }
+                    level_kode: { required: true, minlength: 2, maxlength: 10 },
+                    level_nama: { required: true, minlength: 3, maxlength: 100 }
                 },
                 submitHandler: function (form) {
                     $.ajax({
@@ -73,7 +73,7 @@
                                     title: 'Berhasil',
                                     text: response.message
                                 });
-                                $('#kategori-table').DataTable().ajax.reload();
+                                $('#level-table').DataTable().ajax.reload();
                             } else {
                                 $('.error-text').text('');
                                 $.each(response.msgField, function (prefix, val) {
